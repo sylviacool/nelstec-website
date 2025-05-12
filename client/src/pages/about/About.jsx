ea8e40b04c3fd57ce581191a2ecd06214d18aae9
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2025-05-13 00:22:07 +0200

    Add Styles to make it responsive

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from '../../Components/Header'
+import HeaderImage from '../../assets/images/header_bg_1.png'
 import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <>
+      <Header title="About Us" image={HeaderImage}>
+        At NELSTEC, we specialize in providing cutting-edge technology
+        solutions for businesses of all sizes. With a focus on innovation and
+        reliability, we help companies streamline their operations and drive
+        growth through our tailored software solutions
+      </Header>
+      <h1>About</h1>
+    </>
+  );
 }
 
 export default About
