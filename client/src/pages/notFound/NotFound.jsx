73aacfb1bc15d02a87e020c96a2c619682219c82
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2025-05-14 00:19:38 +0200

    Add notFound,jsx

--- client/src/pages/notFound/NotFound.jsx
+++ client/src/pages/notFound/NotFound.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './notFound.css'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
+    <section>
+      <div className="container not-found_container">
+        <h1>404</h1>
+        <p>Page Not Found.</p>
+        <Link to="/" className="btn">
+          Go back home
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default NotFound
